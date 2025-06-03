914256bd874f88c52ab95e9edeedddceafd65ec8
siva vadde <sivavadde150@gmail.com>
2025-06-03 07:57:49 -0400

    fourth commit

---
+++ Loops/Loop_with_case.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+	case $i in
+		1) echo "this is one ";;
+  		2) echo "this is two ";;
+                3) echo "this is three ";;
+                4) echo "this is four ";;
+                5) echo "this is five ";;
+	esac
+
+done
+
+	
